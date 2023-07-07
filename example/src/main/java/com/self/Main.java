00a9f14e26c7d71d452276daa70f3b259aaf9d63
tianruinan <tianruinan@xylink.com>
2023-07-07 09:30:58 +0800

    2023年07月07日09:29:44 调整目录

--- example/src/main/java/com/self/Main.java
+++ example/src/main/java/com/self/Main.java
@@ -1,4 +1,4 @@
-package example.src.main.java.com.self;
+package com.self;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- example/src/main/java/com/self/config/Configurations.java
+++ example/src/main/java/com/self/config/Configurations.java
@@ -1,4 +1,4 @@
-package example.src.main.java.com.self.config;
+package com.self.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
